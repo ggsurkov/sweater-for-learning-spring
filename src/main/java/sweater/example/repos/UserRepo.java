ff2e9005d6c7857306d6c9e18e3f02b45f633db4
Georgy Surkov <ggsurkov@gmail.com>
2019-03-11 22:51:28 +0300

    mail sender with activated code

--- src/main/java/sweater/example/repos/UserRepo.java
+++ src/main/java/sweater/example/repos/UserRepo.java
@@ -5,4 +5,5 @@ import sweater.example.domain.User;
 
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByUsername(String username);
+    User findByActivationCode(String code);
 }
